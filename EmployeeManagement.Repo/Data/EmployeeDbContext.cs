042dd37653b6da22dc9431f9b8b6ecec07423043
smahfuz <mahfuz.duetcse@gmail.com>
2024-09-13 11:30:45 +0600

    project stracture create

---
+++ EmployeeManagement.Repo/Data/EmployeeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeManagement.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Repo.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeManagement.Web/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeManagement.Core.Models.Base;
+
+namespace EmployeeManagement.Web.ViewModels
+{
+    public class EmployeeViewModel : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhotoPath { get; set; }
+        public IFormFile Images { get; set; }
+    }
+}
